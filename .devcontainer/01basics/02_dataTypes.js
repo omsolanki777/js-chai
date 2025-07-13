1372a51065a071ef211d615dc362a79e7df89b4f
omsolanki777 <omsolanki3005@gmail.com>
2025-07-13 17:18:26 +0000

    Datatypes

---
+++ .devcontainer/01basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; // trat all JS code as newer version
+
+// alert (3 + 3) // we are using Nodejs, not browser
+
+console.log(3
+    +
+     3) // code readability should high
+
+console.log("Solanki")
+
+let name = "Om"      // String
+let age = 22         //int
+let isLoggedIn = false  //boolean
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbole => unique
+
+
+// object
+
+console.log(typeof "Solanki") //str
+console.log(typeof age)       //num
+console.log(typeof null)      //object
+console.log(typeof undefined) //undefined
