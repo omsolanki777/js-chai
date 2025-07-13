80923faadc398d4f514487ceadde4b5c16c4c51d
omsolanki777 <omsolanki3005@gmail.com>
2025-07-13 19:26:39 +0000

    Conversion & Operations

---
+++ .devcontainer/01basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN(Not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "Om"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Om" => true
+
+let someNumber = 77
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+//  ***************** Operations ***************
+ 
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+str1 = "Chat"
+str2 = " Gpt"
+str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1"+ 2 + 2);
+console.log(1 + 2 + "3");
+
+// make a code redability dont make it complex that main aganda of last consol.log
+
+
+
+
+
+
+
